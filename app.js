528ea08fceeaab4b6300f6736e561e01769e32c9
Abdulbaki <abdulliadi3@gmail.com>
2026-05-22 15:35:10 +0100

    changes

--- app.js
+++ app.js
@@ -12,3 +12,27 @@ closeBtn.addEventListener("click", () => {
   navBar.classList.remove("active");
   btns.classList.remove("active");
 });
+
+// cht
+
+const cards = document.querySelectorAll(".testimonial-card");
+const dots = document.querySelectorAll(".dot");
+
+const options = {
+  root: document.querySelector(".testimonial-container"),
+  threshold: 0.6,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const currentCardIndex = Array.from(cards).indexOf(entry.target);
+
+      dots.forEach((dot) => dot.classList.remove("active"));
+
+      dots[currentCardIndex].classList.add("active");
+    }
+  });
+}, options);
+
+cards.forEach((card) => observer.observe(card));
